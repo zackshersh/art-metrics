b441f8e8aa3645cbec691b5393063b45e6b9d7d1
Zack Hersh <zackshersh@gmail.com>
2024-08-12 10:54:27 -0400

    vercel analyics

--- client/src/routes/Root.js
+++ client/src/routes/Root.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import { Analytics } from '@vercel/analytics/react';
+
 function Root(props) {
     return (
         <div>
             <Outlet />
+            <Analytics />
         </div>
     );
 }
